a886d099624b826229935e3cda8cddddaad5bf8a
donioscode <doniyornormuxammedov@gmail.com>
2024-05-07 14:22:37 +0500

    added crolleView file

---
+++ SwiftUIBasics/ScrollViewBasic.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewBasic.swift
+//  SwiftUIBasics
+//
+//  Created by doniyor normuxammedov on 07/05/24.
+//
+
+import SwiftUI
+
+struct ScrollViewBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewBasic()
+}
