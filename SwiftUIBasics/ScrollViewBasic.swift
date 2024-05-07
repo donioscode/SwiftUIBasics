725b9a4f0734a81e5bef0d43bdc6e16d02e617a0
donioscode <doniyornormuxammedov@gmail.com>
2024-05-07 15:49:00 +0500

    some code to ScrollView

--- SwiftUIBasics/ScrollViewBasic.swift
+++ SwiftUIBasics/ScrollViewBasic.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct ScrollViewBasic: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView () {
+            LazyVStack {
+                ForEach(0..<100 ) { index in
+                    ScrollView(.horizontal,showsIndicators: false, content: {
+                        LazyHStack {
+                            ForEach(0..<100) {index in
+                                RoundedRectangle(cornerRadius:  25.0)
+                                    .fill(Color.white)
+                                    .frame(width: 200,height: 150)
+                                    .overlay(
+                                            Text("5")
+                                                .foregroundColor(.white)
+                                                .font(.title)
+                                                .frame(width: 30,height: 30)
+                                                .background(.red)
+                                                .cornerRadius(15),alignment: .bottomTrailing
+                                                 )
+                                    .shadow(radius: 10)
+                                    .padding()
+                            }
+                        }
+                    })
+                }
+            }
+        }
     }
 }
 
