72866099ff6fe6e50c713ecb688cc954c4b9d8ce
donioscode <doniyornormuxammedov@gmail.com>
2024-05-07 13:46:26 +0500

    initializer some code

--- SwiftUIBasics/Initializer-"init()".swift
+++ SwiftUIBasics/Initializer-"init()".swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct Initializer__init___: View {
+struct InitializerBasics: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Initializer__init___()
+    InitializerBasics()
 }
--- SwiftUIBasics/InitializerBasics.swift
+++ SwiftUIBasics/InitializerBasics.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct InitializerBasics: View {
+    
+    let backgroundColor: Color
+    let count: Int
+    let title: String
+    
+    init(count: Int, fruit: Fruits) {
+        self.count = count
+        
+        if fruit == .apple {
+            self.title = "Apple"
+            self.backgroundColor = .red
+        } else {
+            self.title = "Orange"
+            self.backgroundColor = .orange
+        }
+    }
+    enum Fruits {
+        case apple
+        case orange
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 12){
+            Text("\(count)")
+                .font(.largeTitle)
+                .underline()
+                .foregroundColor(.white)
+            Text(title)
+                .foregroundColor(.white)
+                .font(.largeTitle)
+                .font(.headline)
+        }
+        .frame(width: 150,height: 150)
+        .background(backgroundColor)
+        .cornerRadius(20)
+//        .shadow(color: .red, 
+//                radius: 5,
+//                x: 0.0, y: 10)
     }
 }
 
 #Preview {
-    InitializerBasics()
+    HStack {
+        InitializerBasics(count: 11, fruit: .apple)
+        InitializerBasics(count: 55, fruit: .orange)
+    }
 }
