d8dfe475a7d15fc3845934e0ea63cb9898c6459b
donioscode <doniyornormuxammedov@gmail.com>
2024-05-07 13:43:08 +0500

    Initializer

---
+++ SwiftUIBasics/Initializer-"init()".swift
@@ -0,0 +1,18 @@
+//
+//  Initializer-"init()".swift
+//  SwiftUIBasics
+//
+//  Created by doniyor normuxammedov on 07/05/24.
+//
+
+import SwiftUI
+
+struct Initializer__init___: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Initializer__init___()
+}
---
+++ SwiftUIBasics/InitializerBasics.swift
@@ -0,0 +1,18 @@
+//
+//  InitializerBasics.swift
+//  SwiftUIBasics
+//
+//  Created by doniyor normuxammedov on 07/05/24.
+//
+
+import SwiftUI
+
+struct InitializerBasics: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InitializerBasics()
+}
