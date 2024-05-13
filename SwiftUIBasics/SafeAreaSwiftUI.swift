5f9bb2e1f90d5978bdd0ec9eea05ea8523721fd2
donioscode <doniyornormuxammedov@gmail.com>
2024-05-13 15:44:20 +0500

    Added two file

---
+++ SwiftUIBasics/SafeAreaSwiftUI.swift
@@ -0,0 +1,18 @@
+//
+//  SafeAreaSwiftUI.swift
+//  SwiftUIBasics
+//
+//  Created by doniyor normuxammedov on 13/05/24.
+//
+
+import SwiftUI
+
+struct SafeAreaSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SafeAreaSwiftUI()
+}
