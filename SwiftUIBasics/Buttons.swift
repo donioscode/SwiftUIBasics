da4d693bb2c81679094e66ccddc4994cd92fff55
donioscode <doniyornormuxammedov@gmail.com>
2024-05-13 15:45:16 +0500

    add different Buttons and safe areas

--- SwiftUIBasics/Buttons.swift
+++ SwiftUIBasics/Buttons.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct Buttons: View {
+    
+   @State var title = "Hello tittle"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 20) {
+            Text(title)
+            Button("Press me!"){
+                self.title = "First title"
+            }
+            .accentColor(.red)
+            
+            Button {
+                self.title = "Second Btn Pressed"
+            } label: {
+                Text("Save".uppercased())
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(
+                        Color.blue
+                            .cornerRadius(10)
+                            .shadow(radius: 20)
+                    )
+            }
+            
+            Button {
+                self.title = "Third Btn Pressed"
+            } label: {
+                Circle()
+                    .fill(.white)
+                    .frame(width: 100,height: 100)
+                    .overlay(Image(systemName: "heart.fill"))
+                    .font(.largeTitle)
+                    .foregroundColor(.red)
+                    .shadow(radius: 20)
+            }
+            Button {
+                self.title = "#4 way"
+            } label: {
+                Text("Finish")
+                    .font(.caption)
+                    .bold()
+                    .foregroundColor(.black)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(
+                    Capsule()
+                        .stroke(Color.gray,lineWidth: 2.0)
+                    )
+            }
+
+            
+            Button("Hello", systemImage: "heart.fill") {
+                self.title = "#5 press sfefe"
+            }
+
+
+        }
     }
 }
 
--- SwiftUIBasics/SafeAreaSwiftUI.swift
+++ SwiftUIBasics/SafeAreaSwiftUI.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct SafeAreaSwiftUI: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Text("Safe Area Hello uskiy")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                ForEach(0..<20) { index in
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(Color.white)
+                        .frame(height: 150)
+                        .shadow(radius: 10)
+                        .padding(20)
+                }
+                
+            }
+            
+        }
+        .background(
+            Color.blue
+                .ignoresSafeArea(edges: .all)
+        )
     }
 }
 
