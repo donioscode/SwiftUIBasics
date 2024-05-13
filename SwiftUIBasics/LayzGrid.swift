99f1049ae77537287ac57b89ee99db5ffef68f8a
donioscode <doniyornormuxammedov@gmail.com>
2024-05-13 14:28:43 +0500

    added new Grid  file

---
+++ SwiftUIBasics/LayzGrid.swift
@@ -0,0 +1,18 @@
+//
+//  LayzGrid.swift
+//  SwiftUIBasics
+//
+//  Created by doniyor normuxammedov on 13/05/24.
+//
+
+import SwiftUI
+
+struct LayzGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LayzGrid()
+}
