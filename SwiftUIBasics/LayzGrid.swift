df808fc6a56269cf07f7007d587ffb9db9435f35
donioscode <doniyornormuxammedov@gmail.com>
2024-05-13 14:32:29 +0500

    LayzVGrid examples

--- SwiftUIBasics/LayzGrid.swift
+++ SwiftUIBasics/LayzGrid.swift
@@ -9,7 +9,59 @@ import SwiftUI
 
 struct LayzGrid: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let colums: [GridItem] = [
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            
+        ]
+        
+        ScrollView(){
+            Rectangle()
+                .fill(Color.orange)
+                .frame(height: 400)
+            
+            
+            LazyVGrid(
+                columns:colums,
+                alignment: .center,
+                spacing: nil,
+                pinnedViews: [.sectionHeaders],
+                content: {
+                    Section(header:
+                                Text("Section 1 ")
+                        .foregroundColor(Color.white)
+                        .font(.title)
+                        .frame(maxWidth: .infinity,alignment: .leading)
+                        .background(Color.blue)
+                        .padding()
+                            
+                    ){
+                        ForEach(0..<20) { index in
+                            Rectangle()
+                                .frame(height: 150)
+                        }
+                    }
+                    
+                    Section(header:
+                                Text("Section 2 ")
+                        .foregroundColor(Color.white)
+                        .font(.title)
+                        .frame(maxWidth: .infinity,alignment: .leading)
+                        .background(Color.red)
+                        .padding()
+                            
+                    ){
+                        ForEach(0..<20) { index in
+                            Rectangle()
+                                .frame(height: 150)
+                                .foregroundColor(.yellow)
+                                
+                        }
+                    }
+                    
+                })
+        }
     }
 }
 
