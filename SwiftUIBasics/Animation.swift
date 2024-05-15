92377de1f298ffa6ce167143acaed6ed1a296c4f
donioscode <doniyornormuxammedov@gmail.com>
2024-05-15 15:25:02 +0500

    adding to new file to animation

---
+++ SwiftUIBasics/Animation.swift
@@ -0,0 +1,18 @@
+//
+//  Animation.swift
+//  SwiftUIBasics
+//
+//  Created by doniyor normuxammedov on 14/05/24.
+//
+
+import SwiftUI
+
+struct Animation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Animation()
+}
