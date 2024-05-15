8dce62f5e10ba0e262ca6e73f2c76f128b479d40
donioscode <doniyornormuxammedov@gmail.com>
2024-05-15 15:56:12 +0500

    add animation file trasition

---
+++ SwiftUIBasics/Transition.swift
@@ -0,0 +1,18 @@
+//
+//  Transition.swift
+//  SwiftUIBasics
+//
+//  Created by doniyor normuxammedov on 15/05/24.
+//
+
+import SwiftUI
+
+struct Transition: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Transition()
+}
