5102f5b3e3fad7c6ecf910ead1b050bc69ebadeb
donioscode <doniyornormuxammedov@gmail.com>
2024-05-15 16:39:13 +0500

    added new sheets file

---
+++ SwiftUIBasics/PopoverSheetsFullScreeen.swift
@@ -0,0 +1,18 @@
+//
+//  PopoverSheetsFullScreeen.swift
+//  SwiftUIBasics
+//
+//  Created by doniyor normuxammedov on 15/05/24.
+//
+
+import SwiftUI
+
+struct PopoverSheetsFullScreeen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopoverSheetsFullScreeen()
+}
---
+++ SwiftUIBasics/SheetsAndFullScreenCover.swift
@@ -0,0 +1,18 @@
+//
+//  SheetsAndFullScreenCover.swift
+//  SwiftUIBasics
+//
+//  Created by doniyor normuxammedov on 15/05/24.
+//
+
+import SwiftUI
+
+struct SheetsAndFullScreenCover: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetsAndFullScreenCover()
+}
