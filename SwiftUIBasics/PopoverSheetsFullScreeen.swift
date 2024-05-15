5209c999cf062dc7fd6e5c7382fadf4da0ef27c4
donioscode <doniyornormuxammedov@gmail.com>
2024-05-15 17:47:18 +0500

    3 way to use two screen

--- SwiftUIBasics/PopoverSheetsFullScreeen.swift
+++ SwiftUIBasics/PopoverSheetsFullScreeen.swift
@@ -8,11 +8,73 @@
 import SwiftUI
 
 struct PopoverSheetsFullScreeen: View {
+    @State var showScreen:Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.blue
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Button("BUTTON") {
+                    showScreen.toggle()
+                }
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                Spacer()
+            }
+            
+            //Method-1 -- TRANSITION
+//            ZStack {
+//                if showScreen {
+//                    NewScreen(showScreen: $showScreen)
+//                        .padding(.top,500)
+//                        .transition(.move(edge: .bottom))
+//                        .animation(.spring())
+//                }
+//            }
+//            zIndex(2.0)
+           
+            
+            //Method-2 -- SHEETS
+//            .sheet(isPresented: $showScreen, content: {
+//                NewScreen()
+//            })
+            
+            //Method-3 -- Animation OFFSET
+            NewScreen(showScreen: $showScreen)
+                .padding(.top,100)
+                .offset(y: showScreen ? 0 : UIScreen.main.bounds.height)
+                .animation(.spring())
+        }
     }
 }
 
+
+struct NewScreen: View {
+    @Binding var showScreen: Bool
+//   Method-2  @Environment(\.presentationMode) var presentationMode
+    var body: some View {
+        ZStack(alignment: .topLeading){
+            Color.yellow
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Button(action: {
+                    //method 1 and 3
+                    showScreen.toggle()
+//                  Method-2   presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    Image(systemName: "xmark")
+                        .font(.largeTitle)
+                        .padding(20)
+                })
+            }
+            
+            
+        }
+    }
+}
 #Preview {
     PopoverSheetsFullScreeen()
+//    NewScreen()
 }
