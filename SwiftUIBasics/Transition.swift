c9470f886a0edaaa397655b12711c1d831bc9707
donioscode <doniyornormuxammedov@gmail.com>
2024-05-15 15:57:17 +0500

    transition animation acymmetric

--- SwiftUIBasics/Transition.swift
+++ SwiftUIBasics/Transition.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct Transition: View {
+    @State var showView: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment:.bottom){
+            VStack{
+                Button("Button") {
+                    showView.toggle()
+                }
+                Spacer()
+            }
+            
+            if showView {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .frame(height: UIScreen.main.bounds.height * 0.5)
+//                    .opacity(showView ? 1.0 : 0.0)
+//                    .transition(.move(edge: .bottom))
+                    .transition(.asymmetric(
+                        insertion: .move(edge: .bottom),
+                        removal: AnyTransition.opacity.animation(.easeInOut)))
+                    .animation(.easeInOut)
+            }
+           
+        }
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
