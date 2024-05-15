3bd7b066ebe2eb499ea279caaf47c66f4d89de35
donioscode <doniyornormuxammedov@gmail.com>
2024-05-15 16:40:06 +0500

    example sheets two screen

--- SwiftUIBasics/SheetsAndFullScreenCover.swift
+++ SwiftUIBasics/SheetsAndFullScreenCover.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct SheetsAndFullScreenCover: View {
+    @State var showSheets: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.green
+            VStack {
+                Button(action: {
+                    showSheets.toggle()
+                }, label: {
+                    Text("Button")
+                        .foregroundColor(.green)
+                        .padding(20)
+                        .background(Color.white.cornerRadius(10))
+                })
+                .sheet(isPresented: $showSheets, content: {
+                    SecondView()
+                })
+//                .fullScreenCover(isPresented: $showSheets, content: {
+//                    SecondView()
+//                })
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+    
+struct SecondView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Color.red
+            VStack {
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .padding(20)
+                    
+                    
+                })
+                
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
-
-#Preview {
-    SheetsAndFullScreenCover()
 }
+    
+    #Preview {
+        SheetsAndFullScreenCover()
+    }
+
